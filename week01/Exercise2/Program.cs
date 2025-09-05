374bd2e8d2d66499fd9d99326f30b9b093919cec
osoko odunayo <osokohordunayo@gmail.com>
2025-09-05 20:03:24 +0100

    updating the exercise 2 project on conditionals

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+            if (grade >= 97)
+            {
+                Console.WriteLine("Your grade is A+");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is A");
+            }
+        else if (grade >= 80)
+            if (grade >= 87)
+            {
+                Console.WriteLine("Your grade is B+");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is B");
+            }
+        else if (grade >= 70)
+            if (grade >= 77)
+            {
+                Console.WriteLine("Your grade is C+");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is C");
+            }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
+         
+    
 }
