93294c1af07e59502e924f0ad2e29d36312764f6
osoko odunayo <osokohordunayo@gmail.com>
2025-09-06 03:18:42 +0100

    completing exercise 4 - loop project

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int newNumber = -1;
+
+        while (newNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string response = Console.ReadLine();
+            newNumber = int.Parse(response);
+
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+        int sum = 0;
+        // calculating the sum
+        foreach (int number in numbers)
+        {
+            sum += number;
+
+        }
+        // computing the sum
+        Console.WriteLine($"The sum is: {sum}");
+        // calculating the average
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average number is: {average}");
+        // calculating the maximum number
+        int max = -1;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");     
     }
+
 }
