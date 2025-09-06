9f8a76c3dd6a4ab8894c414609cb5a5201fe3df9
osoko odunayo <osokohordunayo@gmail.com>
2025-09-06 03:57:11 +0100

    completing exercise 5 - finctions project

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+        string username = PromptUsername();
+        int favouriteNumber = PromptNumber();
+        int square = SquaredNumber(favouriteNumber);
+        DisplayDetails(username, square);
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the program.");
+        }
+
+        static string PromptUsername()
+        {
+            Console.Write("Please, Enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptNumber()
+        {
+            Console.Write("Please, Enter your favourite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquaredNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayDetails(string username, int square)
+        {
+            Console.Write($"{username}, the square of your number is: {square}");
+        }
     }
 }
