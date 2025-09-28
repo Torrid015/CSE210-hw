d800a98103e2c9a4ac7e6c6d3f54ad12b24c1ac1
osoko odunayo <osokohordunayo@gmail.com>
2025-09-28 20:05:35 +0100

    Add Order and Product classes for online ordering system
    - Implemented Order class to manage customer orders, including methods to add products and display order details.
    - Created Product class to represent individual products with properties for name, product ID, price, and quantity.
    - Added functionality to calculate total cost of products and display packing and shipping labels in a formatted manner.

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+       
+   
+        // Customers and addresses
+        Address addr1 = new Address("63rd St", "Chicago", "CH", "USA");
+        Customer cust1 = new Customer("David Radcliff", addr1);
+
+        Address addr2 = new Address("54 S ", "North Carolina", "NC", "USA");
+        Customer cust2 = new Customer("Mary Williams", addr2);
+
+        // Orders
+        Order order1 = new Order(cust1);
+        order1.AddProduct(new Product("Laptop", "LAP01", 2, 5));
+        order1.AddProduct(new Product("Rose", "ROSE02", 2, 4));
+
+        Order order2 = new Order(cust2);
+        order2.AddProduct(new Product("Noodles", "NOODLES03", 7, 2));
+        order2.AddProduct(new Product("Conditioner", "COND04", 4, 3));
+
+        // Display orders
+        order1.DisplayOrder();
+        order2.DisplayOrder();
     }
 }
